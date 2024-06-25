8481638da2e1aa9ed2314855106cf5d67a22dda0
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2024-06-25 22:56:04 +0100

    fix: add tailwind css

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <section
+    class="bg-dark-blue flex justify-center items-center h-screen w-screen text-white"
+  >
+    <h1 class="text-4xl text-bold">Test</h1>
+  </section>
+</template>
