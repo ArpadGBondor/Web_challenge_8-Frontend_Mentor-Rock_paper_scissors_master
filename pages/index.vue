80e2e3c96aeb5259a2b54d8b5d0a1cef16f81eee
Arpad G. Bondor [Gabriel] <arpad.g.bondor@gmail.com>
2024-07-04 01:38:14 +0100

    fix: add game logic

--- pages/index.vue
+++ pages/index.vue
@@ -13,16 +13,6 @@
             v-model="message"
           />
         </div>
-        <div class="flex justify-between items-center w-full">
-          <label class="m-4" for="increment">Add Score</label>
-          <input
-            type="button"
-            name="increment"
-            value="Increment"
-            @click="onIncrement"
-            class="bg-white text-score-text m-4 p-2"
-          />
-        </div>
       </form>
     </div>
     <div class="m-2 p-2 border-[1px] border-white flex flex-row gap-4">
@@ -40,20 +30,81 @@
       <Disk size="lg" type="paper" />
       <Disk size="lg" type="scissors" />
     </div>
+    <div class="text-2xl text-center">
+      <h2>Game Store</h2>
+      <p>score: {{ score }}</p>
+      <p>stage: {{ stage }}</p>
+      <p>player choice: {{ player }}</p>
+      <p>computer choice: {{ computer }}</p>
+      <p>game outcome: {{ outcome }}</p>
+    </div>
+    <div>
+      <h2>Actions:</h2>
+      <div class="flex justify-between items-center w-full">
+        <label class="m-4" for="newGame"
+          >Start New Game (only in stage 4)</label
+        >
+        <input
+          type="button"
+          name="newGame"
+          value="Start New Game"
+          @click="onNewGame"
+          class="bg-white text-score-text m-4 p-2"
+        />
+      </div>
+      <div class="flex justify-between items-center w-full">
+        <label class="m-4" for="chooseRock"
+          >Choose Rock (only in stage 1)</label
+        >
+        <input
+          type="button"
+          name="chooseRock"
+          value="Choose Rock"
+          @click="onRockChoice"
+          class="bg-white text-score-text m-4 p-2"
+        />
+      </div>
+      <div class="flex justify-between items-center w-full">
+        <label class="m-4" for="choosePaper"
+          >Choose Paper (only in stage 1)</label
+        >
+        <input
+          type="button"
+          name="choosePaper"
+          value="Choose Paper"
+          @click="onPaperChoice"
+          class="bg-white text-score-text m-4 p-2"
+        />
+      </div>
+      <div class="flex justify-between items-center w-full">
+        <label class="m-4" for="chooseScissors"
+          >Choose Scissors (only in stage 1)</label
+        >
+        <input
+          type="button"
+          name="chooseScissors"
+          value="Choose Scissors"
+          @click="onScissorsChoice"
+          class="bg-white text-score-text m-4 p-2"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Disk } from '#components';
+import { storeToRefs } from 'pinia';
 import { useNotifyStore, NotificationType } from '../stores/notify.store';
-import { useGameStore } from '../stores/game.store';
+import { useGameStore, ChoiceType } from '../stores/game.store';
+
+const notifyStore = useNotifyStore();
+const gameStore = useGameStore();
+const { score, stage, player, computer, outcome } = storeToRefs(gameStore);
 
 definePageMeta({
   layout: 'default',
 });
-
-const notifyStore = useNotifyStore();
-const gameStore = useGameStore();
 
 let message = '';
 
@@ -62,7 +113,10 @@ const onSubmit = (event: any) => {
 
   notifyStore.notify(message, NotificationType.Error);
 };
-const onIncrement = () => {
-  gameStore.increment();
-};
+const onIncrement = () => gameStore.incrementScore();
+const onDecrement = () => gameStore.decrementScore();
+const onNewGame = () => gameStore.startNewGame();
+const onPaperChoice = () => gameStore.playerMove(ChoiceType.Paper);
+const onRockChoice = () => gameStore.playerMove(ChoiceType.Rock);
+const onScissorsChoice = () => gameStore.playerMove(ChoiceType.Scissors);
 </script>
--- stores/game.store.ts
+++ stores/game.store.ts
@@ -1,21 +1,107 @@
 import { defineStore } from 'pinia';
 
+export enum ChoiceType {
+  None = 'none',
+  Rock = 'rock',
+  Paper = 'paper',
+  Scissors = 'scissors',
+}
+
+export enum OutcomeType {
+  None = 'none',
+  Won = 'won',
+  Lost = 'lost',
+  Draw = 'draw',
+}
+
+export enum StageType {
+  PlayerChoice = '1',
+  ComputerChoice = '2',
+  Results = '3',
+  Over = '4',
+}
+
 interface State {
   score: number;
   showRules: boolean;
+  stage: StageType;
+  player: ChoiceType;
+  computer: ChoiceType;
+  outcome: OutcomeType;
 }
 
 export const useGameStore = defineStore('game', {
   state: (): State => ({
     score: 0,
     showRules: false,
+    stage: StageType.PlayerChoice,
+    player: ChoiceType.None,
+    computer: ChoiceType.None,
+    outcome: OutcomeType.None,
   }),
   actions: {
-    increment() {
-      this.score++;
-    },
     setShowRules(_showRules: boolean) {
       this.showRules = _showRules;
+    },
+    startNewGame() {
+      if (this.stage !== StageType.Over) return;
+      this.stage = StageType.PlayerChoice;
+      this.player = ChoiceType.None;
+      this.computer = ChoiceType.None;
+      this.outcome = OutcomeType.None;
+    },
+    playerMove(move: ChoiceType) {
+      if (this.stage !== StageType.PlayerChoice) return;
+      this.player = move;
+      this.stage = StageType.ComputerChoice;
+
+      const computerMove = () => {
+        const choices = [
+          ChoiceType.Rock,
+          ChoiceType.Paper,
+          ChoiceType.Scissors,
+        ];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        this.computer = choices[randomIndex];
+        this.stage = StageType.Results;
+
+        const showOutcome = () => {
+          if (
+            (this.player === ChoiceType.Rock &&
+              this.computer === ChoiceType.Paper) ||
+            (this.player === ChoiceType.Paper &&
+              this.computer === ChoiceType.Scissors) ||
+            (this.player === ChoiceType.Scissors &&
+              this.computer === ChoiceType.Rock)
+          ) {
+            this.outcome = OutcomeType.Lost;
+            this.score--;
+          } else if (
+            (this.player === ChoiceType.Rock &&
+              this.computer === ChoiceType.Scissors) ||
+            (this.player === ChoiceType.Paper &&
+              this.computer === ChoiceType.Rock) ||
+            (this.player === ChoiceType.Scissors &&
+              this.computer === ChoiceType.Paper)
+          ) {
+            this.outcome = OutcomeType.Won;
+            this.score++;
+          } else if (
+            (this.player === ChoiceType.Rock &&
+              this.computer === ChoiceType.Rock) ||
+            (this.player === ChoiceType.Paper &&
+              this.computer === ChoiceType.Paper) ||
+            (this.player === ChoiceType.Scissors &&
+              this.computer === ChoiceType.Scissors)
+          ) {
+            this.outcome = OutcomeType.Draw;
+          }
+          this.stage = StageType.Over;
+        };
+
+        setTimeout(showOutcome, 2000);
+      };
+      setTimeout(computerMove, 2000);
     },
   },
 });
